d11db01f91deac39350b357495fabbbf51044695
aldoaquino451 <aldoaquino2909@gmail.com>
2024-01-10 18:04:57 +0100

    [edit] app

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,47 @@ export default{
   name: 'App',
   data() {
     return {
-      saluto: 'ciao'
+      login: 'http://127.0.0.1:8000/login',
     }
   },
 }
 </script>
 
 <template>
-  <header>
-  <div>
-    <p class="fs-1">{{ saluto }}</p>
-    <a href="http://127.0.0.1:8000/login">Login</a>
-  </div>
+  <header class="bg-dark text-light py-2">
+    <div class="container h-100 d-flex justify-content-between  align-items-center">
+      <span class="fs-1">Header</span>
+        <a class="btn btn-primary" :href="login">Login</a>
+    </div>
   </header>
+
+  <main class="py-4">
+    <div class="container d-flex flex-column align-items-center">
+      <h1>titolo</h1>
+      <div>
+      </div>
+    
+    </div>
+  </main>
+
+  <footer  class="bg-dark text-light py-2">
+    <div>Chi siamo</div>
+    <div>Info</div>
+    <div>Social</div>
+  </footer>
 </template>
 
 <style>
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+header {
+  height: 80px;
+}
+main {
+  flex-grow: 2;
+}
 
 </style>
