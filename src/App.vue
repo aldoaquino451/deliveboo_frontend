4dd41ecd30c030a8dfd62d51a7e32f663172646e
aldoaquino451 <aldoaquino2909@gmail.com>
2024-01-10 13:07:47 +0100

    [add] scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default{
+  name: 'App',
+  data() {
+    return {
+      saluto: 'ciao'
+    }
+  },
+}
 </script>
 
 <template>
+  <header>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <p class="fs-1">{{ saluto }}</p>
+    <a href="http://127.0.0.1:8000/login">Login</a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  </header>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+
 </style>
