2cab6cffa9713140d06ae461f6a68f3502468742
Vahidone <v.ban.roma@gmail.com>
2024-01-18 13:30:21 +0100

    [feat] add restaurant detail page

--- src/pages/DetailRestaurant.vue
+++ src/pages/DetailRestaurant.vue
@@ -11,8 +11,9 @@ components:{
 
   data() {
     return {
-      restaurant:{}
-
+      restaurant:{},
+      categories: [],
+      products: []
     }
   },
 
@@ -24,8 +25,20 @@ components:{
         // if(!res.data.restaurant){
         //   this.$router.push({name: 'error-404'})
         // }
-        console.log(res)
+        console.log(res.data)
         this.restaurant = res.data;
+
+        this.products = res.data.products;
+        console.log(this.products);
+        this.products.forEach(product =>{
+          const categoryName = product.category.name;
+          if(!this.categories.includes(categoryName)) {
+
+            this.categories.push(categoryName);
+          }
+          
+        })
+        
       })
     },
 
@@ -43,17 +56,38 @@ components:{
 </script>
 
 <template>
-  <section>
-    <h1>dettaglio ristorante e menu</h1>
-    <h2>{{ restaurant.name_restaurant }}</h2>
-  </section>
+  <main>
+    <div class="container">
+      <router-link class="btn btn-primary my-5" :to="{name: 'home' }">Torna </router-link>
+      <h1>{{ restaurant.name_restaurant }}</h1>
+      <span class="fw-bold">Tipologia: </span><span class="mx-5" v-for="typology in restaurant.typologies" :key="typology.id">{{ typology.name }}</span>
+      <p class="mt-5"><span class="fw-bold">Descrizione:</span> {{ restaurant.description }}</p>
+
+      <div v-for="category in categories" :key="category.id">
+        <h2>{{category}}</h2>
+
+        <div v-for="product in products" :key="product.id">
+          <div v-if="product.category.name === category">
+
+            <h3>{{product.name}}</h3>
+            <p><span class="fw-bold">Ingredienti:</span>{{product.ingredints}}</p>
+            <p><span class="fw-bold">Prezzo: </span>€ {{ product.price}}</p>
+            <p v-if="product.is_vegan">prodotto vegano</p>
+            <button class="btn btn-success">+</button>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+ 
+  </main>
 
 </template>
 
 <style lang="scss" scoped>
-  section{
+  main {
     margin-top: 70px;
-    height: 793px;
+  
     h1{
       text-align: center;
     }
