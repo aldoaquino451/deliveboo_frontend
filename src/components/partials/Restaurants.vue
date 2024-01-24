dc03c79b3541ce169ad0c69f73ba230c38faef26
Alematri <alessandromatricardi@gmail.com>
2024-01-24 21:54:39 +0100

    [api paginator]

--- src/components/partials/Restaurants.vue
+++ src/components/partials/Restaurants.vue
@@ -30,6 +30,10 @@ export default{
       
     <p v-else class="text-center">Non ci sono ristoranti per le tipologie scelte!</p>
 
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+
   </div>
 </template>
 
