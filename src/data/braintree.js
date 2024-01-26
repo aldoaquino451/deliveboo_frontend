609c2111b38bfe54699438bac73023a33b6be2aa
MassimoAzzini <massimo.azzini.web@gmail.com>
2024-01-26 14:01:05 +0100

    [edit] remove braintree.js

--- src/data/braintree.js
+++
@@ -1,6 +0,0 @@
-// import { reactive } from 'vue';
-// // import Vue from 'vue'
-// import vue-braintree from 'vue-braintree'
-// // import vueBraintree from 'vue-braintree'
-
-// Vue.use(vue-braintree)
