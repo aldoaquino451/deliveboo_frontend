2f1a0c2fa3218d239741953fca71548fc8b6d146
MassimoAzzini <massimo.azzini.web@gmail.com>
2024-01-26 12:23:00 +0100

    [add] drop-in braintree

--- src/pages/PaymentBraintree.vue
+++ src/pages/PaymentBraintree.vue
@@ -1,6 +1,12 @@
 <script>
 import axios from 'axios';
-import { store } from '../data/store'; 
+import { store } from '../data/store';
+import { router } from '../router'
+
+// numero carte per prove
+// - 4111111111111111  pagamento successo
+// - 4000111111111115  pagamento negato
+
 
 export default {
   name: 'PaymentBraintree',
@@ -8,122 +14,60 @@ export default {
     return {
       tokenApi: '',
       // braintree
-      store
+      store,
+      router
     }
   },
 
   methods: {
 
-    getToken(){
-      axios.get('http://127.0.0.1:8000/api/orders/generate')
-        .then(results => {
-          this.tokenApi = results.data.token;
-        })
-    },
-
-    getMakePayment(){
-      axios.post('http://127.0.0.1:8000/api/orders/make-payment')
-      .then(results => {
-          console.log(results)
-          // this.product = results.data;
-        })
-
-
-    },
-
-    braintreeForm(){
-      const form = document.getElementById('payment-form');
+    formBraintree() {
+      var submitButton = document.querySelector('#submit-button');
 
       braintree.dropin.create({
-        authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b',
-        selector: '#dropin-container',
-        locale: 'it_IT'
-      }, function (err, instance) {
-        if (err) console.error(err);
+        authorization: 'sandbox_7b574dqm_b22n4gtx5t4h7dc7',
+        container: '#dropin-container'
+      }, function (err, dropinInstance) {
+        if (err) {
+          // Handle any errors that might've occurred when creating Drop-in
+          console.error(err);
+          return;
+        }
+        submitButton.addEventListener('click', function () {
+          dropinInstance.requestPaymentMethod(function (err, payload) {
+            if (err) {
+              // Handle errors in requesting payment method
+            }
+            
+            setTimeout(() => {
+              router.push({ name: 'postpayment' });
+            }, 1000);    
 
-        form.addEventListener('submit', function () {
-          
-          instance.requestPaymentMethod(function (err, payload) {
-            if (err) console.error(err);
-
-            // Step four: when the user is ready to complete their
-            //   transaction, use the dropinInstance to get a payment
-            //   method nonce for the user's selected payment method, then add
-            //   it a the hidden field before submitting the complete form to
-            //   a server-side integration
-
-            document.getElementById('nonce').value = payload.nonce;
-            form.submit();          
           });
-        })
-      });    
+        });
+      });
     },
-
   },
 
   mounted() {
-    // this.getToken()
-    this.braintreeForm()
-    // this.getMakePayment()
+    this.formBraintree()
   },
  
 }
 
 </script>
 
+
 <template>
   <div class="container">
     <h1>PaymentBraintree</h1>
-    {{ this.tokenApi }}
-
-     <div class="row">
-       <div class="col-md-8 col-md-offset-2">
-          <form id="payment-form" :action="this.store.apiUrl + 'orders/make-payment'" method="post">
-            <div id="dropin-container"></div>
-            <input type="submit" />
-            <input type="hidden" id="nonce" name="payment_method_nonce" />
-          </form>
-        </div>
-     </div>
+    <div id="dropin-container"></div>
+    <button id="submit-button">Purchase</button>
   </div>
-
 </template>
 
 <style lang="scss" scoped>
 
-.button {
-  cursor: pointer;
-  font-weight: 500;
-  left: 3px;
-  line-height: inherit;
-  position: relative;
-  text-decoration: none;
-  text-align: center;
-  border-style: solid;
-  border-width: 1px;
-  border-radius: 3px;
-  // -webkit-appearance: none;
-  // -moz-appearance: none;
-  display: inline-block;
-}
-
-.button--small {
-  padding: 10px 20px;
-  font-size: 0.875rem;
-}
-
-.button--green {
-  outline: none;
-  background-color: #64d18a;
-  border-color: #64d18a;
-  color: white;
-  transition: all 200ms ease;
-}
-
-.button--green:hover {
-  background-color: #8bdda8;
-  color: white;
-}
 
 
 </style>
