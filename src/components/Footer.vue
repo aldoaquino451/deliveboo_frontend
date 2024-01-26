ec6dbcdcd674c5b033cf2468b702d4b5578b35f6
Vahidone <v.ban.roma@gmail.com>
2024-01-26 15:35:13 +0100

    [add] add footer and change style of paginator btn

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -18,8 +18,56 @@ export default{
 
   <footer class="py-3 bg-dark text-light">
     <div class="container">
+      <div class="mb-4">
+        <h1>Deliveboo</h1>
+      </div>
 
-      <h4 class="m-0 text-center">by Team 5</h4>
+     <div class="row">
+      <div class="col text-center">
+        <h3>Uniamo le forze</h3>
+        <ul>
+          <li><a href="#">Lavora con noi</a></li>
+          <li><a href="#">Deliveboo per i Partner</a></li>
+          <li><a href="#">Corrieri</a></li>
+          <li><a href="#">Deliveboo Business</a></li>
+        </ul>
+      </div>
+      <div class="col text-center">
+        <h3>Link di interesse</h3>
+        <ul>
+          <li><a href="#">Chi siamo</a></li>
+          <li><a href="#">FAQ</a></li>
+          <li><a href="#">Deliveboo Prime</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Contattaci</a></li>
+          <li><a href="#">Sicurezza</a></li>
+        </ul>
+      </div>
+      <div class="col text-center">
+        <h3>Seguici</h3>
+        <ul>
+          <li><a href="#"><i class="fa-brands fa-square-facebook"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-square-instagram"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+ 
+        </ul>
+      </div>
+
+      <div class="col text-center">
+        <h3>Modulistica</h3>
+        <ul>
+          <li><a href="#">Termini e condizioni</a></li>
+          <li><a href="#">Politica sulla privacy</a></li>
+          <li><a href="#">Politica sui cookie</a></li>
+          <li><a href="#">Conformità</a></li>
+        </ul>
+      </div>
+     </div>
+
+     <div class="info">
+      <p class="our-group text-center">By Group 5</p>
+     </div>
 
     </div>
   </footer>
@@ -27,5 +75,28 @@ export default{
 </template>
 
 <style lang="scss" scoped>
+footer {
+  height: 400px;
+  h1 {
+    margin-left: 55px;
+  }
+  li {
+    a {
+      color: rgb(162, 161, 161);
+      text-decoration: none;
+      &:hover {
+        color: white;
+      }
+    }
+  }
 
+  .info {
+    padding-top: 60px;
+  }
+
+  .our-group {
+    color: rgb(162, 161, 161);
+    font-size: 14px;
+  }
+}
 </style>
--- src/components/partials/Paginator.vue
+++ src/components/partials/Paginator.vue
@@ -13,20 +13,52 @@ export default{
 
 <template>
 
-  <div class="container"> 
+<div class="custom-paginator">
     <button
       v-for="(link, index) in links"
       @click="$emit('getPage', link.url)"
       :key="index"
       :disabled="link.active || !link.url"
       v-html="link.label"
-      class="btn btn-outline-primary"
+      class="btn"
+      :class="{ 'btn-outline-danger': !link.active, 'btn-danger': link.active }"
     ></button>
   </div>
 
 </template>
 
 
-<style lang="scss">
+<style lang="scss" scoped>
+
+.custom-paginator {
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+}
+
+.btn {
+  margin: 0 3px;
+  padding: 8px 12px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s, color 0.3s;
+}
+
+.btn-outline-primary {
+  color: #F2542F;
+  background-color: transparent;
+  border: 1px solid #F2542F;
+}
+
+.btn-primary {
+  color: #fff;
+  background-color: #F2542F;
+  border: 1px solid #F2542F;
+}
+
+.btn:disabled {
+  cursor: not-allowed;
+  opacity: 0.6;
+}
 
 </style>
